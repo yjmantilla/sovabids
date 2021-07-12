03782be4b162697011e27587887b78ac7174f94b
Yorguin José Mantilla Ramos <36543115+yjmantilla@users.noreply.github.com>
2021-07-12 15:43:47 -0500

    changed api for schema (use functions instead of variables so that we can have docstrings)

--- sovabids/schemas/__init__.py
+++ sovabids/schemas/__init__.py
@@ -1,7 +1,18 @@
 import os
 this_dir = os.path.dirname(__file__)
 
-bidsmap_sova2coin_file = os.path.join(this_dir,'bidsmap_sova2coin.yml')
-with open(bidsmap_sova2coin_file) as f:
-    bidsmap_sova2coin = f.read()
+def get_sova2coin_bidsmap():
+    """Get the sova2coin bidsmap.
+
+    Returns:
+    --------
+
+    str :
+        The sova2coin bidsmap.
+
+    """
+    bidsmap_sova2coin_file = os.path.join(this_dir,'bidsmap_sova2coin.yml')
+    with open(bidsmap_sova2coin_file) as f:
+        bidsmap_sova2coin = f.read()
+    return bidsmap_sova2coin
 

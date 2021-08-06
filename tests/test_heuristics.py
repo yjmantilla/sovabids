cf65d786ac2afa603f0e0fe8dda7eeb9b61f504e
Yorguin José Mantilla Ramos <36543115+yjmantilla@users.noreply.github.com>
2021-08-06 00:31:37 -0500

    test function

--- tests/test_heuristics.py
+++ tests/test_heuristics.py
@@ -2,7 +2,7 @@ from numpy import source
 from test_bids import dummy_dataset
 from sovabids.heuristics import from_io_example
 
-if __name__ == '__main__':
+def test_from_io_example():
     mappings = dummy_dataset()
 
     sourcepath=mappings[0]['IO']['source']
@@ -10,5 +10,6 @@ if __name__ == '__main__':
     answer=mappings[0]['non-bids']['path_analysis']['pattern']
     pattern = from_io_example(sourcepath,targetpath)
     assert pattern == answer
-
+if __name__ == '__main__':
+    test_from_io_example()
     print('ok')

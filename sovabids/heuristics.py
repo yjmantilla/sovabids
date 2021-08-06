9829c65753d6a4db0adeedae8ec23ced8b66f1c6
Yorguin José Mantilla Ramos <36543115+yjmantilla@users.noreply.github.com>
2021-08-06 00:26:02 -0500

    added a heuristic

---
+++ sovabids/heuristics.py
@@ -0,0 +1,21 @@
+from sovabids.parsers import parse_path_pattern_from_entities
+from sovabids.parsers import parse_entities_from_bidspath
+
+def from_io_example(sourcepath,targetpath):
+    """Get the path pattern from a source-target mapping example.
+    
+    Parameters
+    ----------
+    sourcepath : str
+        The sourcepath that will be modified to get the path pattern
+    targetpath : str
+        The bidspath we are going to derive the information on.
+
+    Returns
+    -------
+    str :
+        The path pattern in placeholder format.
+    """
+    bids_entities=parse_entities_from_bidspath(targetpath)
+    pattern = parse_path_pattern_from_entities(sourcepath,bids_entities)
+    return pattern 

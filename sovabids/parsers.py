d1fedf715b06338c75bb85d7d72c64f77624b446
Yorguin José Mantilla Ramos <36543115+yjmantilla@users.noreply.github.com>
2021-08-06 00:27:20 -0500

    learning from example backend

--- sovabids/parsers.py
+++ sovabids/parsers.py
@@ -1,6 +1,7 @@
 """Module with parser utilities."""
 import re
-
+from copy import deepcopy
+from sovabids.datasets import _modify_entities_of_placeholder_pattern
 from sovabids.misc import flat_paren_counter
 from sovabids.dicts import deep_merge_N,nested_notation_to_tree
 
@@ -96,3 +97,124 @@ def parse_from_regex(string,pattern,fields):
         d = nested_notation_to_tree(field,value)
         l.append(d)
     return deep_merge_N(l)
+
+def parse_entity_from_bidspath(path,entity,mode='r2l'):
+    """Get the value of a bids-entity from a path.
+    
+    Parameters
+    ----------
+    path : str
+        The bidspath we are going to derive the information on.
+        Should be the complete path of file of a modality (ie an _eeg file).
+    entity : str
+        The entity we are going to extract.
+        SHOULD be one of sub|ses|task|acq|run 
+    mode : str
+        Direction of lookup. One of r2l|l2r .
+        r2l (right to left)
+        l2r (left to right)
+
+    Returns
+    -------
+    value : str
+        The extracted value of the entity as a string.
+        If None, it means the entity was not found on the string.
+    """
+    entity = entity if '-' in entity else entity + '-'
+    # Easier to find it from the tail of the bidspath
+    if mode == 'r2l':
+        entity_position = path.rfind(entity)
+    elif mode == 'l2r':
+        entity_position = path.find(entity)
+    else:
+        raise ValueError('Incorrect usage of the mode argument.')
+
+    if entity_position == -1:
+        return None
+
+    little_path = path[entity_position:]
+
+    value = re.search('%s(.*?)%s' % ('-', '_'), little_path,).group(1)
+
+    return value
+
+def parse_entities_from_bidspath(targetpath,entities=['sub','ses','task','acq','run'],mode='r2l'):
+    """Get the bids entities from a bidspath.
+    
+    Parameters
+    ----------
+    targetpath : str
+        The bidspath we are going to derive the information on.
+    entities : list of str
+        The entities we are going to extract.
+        Defaults to sub,ses,task,acq,run
+    mode : str
+        Direction of lookup. One of r2l|l2r .
+        r2l (right to left)
+        l2r (left to right)
+
+    Returns
+    -------
+    dict
+        A dictionary with the extracted entities.
+        {'sub':'11','task':'resting','ses':'V1','acq':'A','run':1}
+    """
+    path = deepcopy(targetpath)
+    bids_dict = dict()
+    for entity in entities:
+        bids_dict[entity] = parse_entity_from_bidspath(path,entity,mode)
+    # Clean Non Existent key
+    bids_dict2 = {key:value for key,value in bids_dict.items() if value is not None}
+    return bids_dict2
+
+def parse_path_pattern_from_entities(sourcepath,bids_entities):
+    """Get the path pattern from a path and a dictionary of bids entities and their values.
+
+    Parameters
+    ----------
+    sourcepath : str
+        The sourcepath that will be modified to get the path pattern
+    bids_entities : dict
+        Dictionary with the entities and their values on the path.
+        Ie {'sub':'11','task':'resting','ses':'V1','acq':'A','run':1}
+        There should be no ambiguity between the sourcepath and each of the values.
+        Otherwise an error will be raised.
+
+    Returns
+    -------
+
+    str :
+        The path pattern in placeholder format
+    """
+    path = deepcopy(sourcepath)
+    values = [val for key,val in bids_entities.items()]
+    key_map={
+        'sub':'%subject%',
+        'ses':'%session%',
+        'task':'%task%',
+        'acq':'%acquisition%',
+        'run':'%run%'
+    }
+    assert '%' not in path # otherwise it will mess up the logic
+    for key,val in bids_entities.items():
+        pathcopy = deepcopy(path)
+        # Replace all other values which are superstrings of the current one
+        superstrings = [x for x in values if val in x and val!=x]
+        for string in superstrings:
+            pathcopy = pathcopy.replace(string,'*'*len(string))
+        # handle ambiguity
+        if pathcopy.count(val) > 1:
+            raise ValueError('Ambiguity: The path has multiple instances of {}'.format(val))
+        if pathcopy.count(val) < 1:
+            raise ValueError('There is no {} in path'.format(val))
+        path = path.replace(val,key_map[key])
+        values[values.index(val)] = key_map[key]
+    path = _modify_entities_of_placeholder_pattern(path)
+    path = path.replace('\\','/')
+    # Find first changing value and put the pattern from there
+    first_placeholder  = path.find('%')
+    # Identify where should the pattern start
+    start = path[:first_placeholder].rfind('/') + 1 if '/' in path[:first_placeholder] else 0
+    path = path[start:]
+    return  path
+

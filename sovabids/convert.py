42e6a0bd65c820a04da4b0cf40babc9c414f133a
Yorguin José Mantilla Ramos <36543115+yjmantilla@users.noreply.github.com>
2021-08-11 23:53:57 -0500

    added more logging to convert_them

--- sovabids/convert.py
+++ sovabids/convert.py
@@ -3,10 +3,14 @@
 import argparse
 import os
 
+import logging
+from sovabids.dicts import deep_get
 from sovabids.rules import load_rules,apply_rules_to_single_file
 from sovabids.bids import update_dataset_description
 from sovabids.loggers import setup_logging
 from sovabids.settings import SECTION_STRING
+
+LOGGER = logging.getLogger(__name__)
 
 def convert_them(mappings_input):
     """Convert eeg files to bids according to the mappings given.
@@ -30,27 +34,40 @@ def convert_them(mappings_input):
     mapping_file = mappings_input if isinstance(mappings_input,str) else None
     
     # Verifying Mappings
-    assert 'Individual' in mappings
-    assert 'General' in mappings
+    assert 'Individual' in mappings,f'`Individual` does not exist in the mapping dictionary'
+    assert 'General' in mappings,f'`General` does not exist in the mapping dictionary'
     
     # Getting input,output and log path
     bids_path = mappings['General']['IO']['target']
-    rawfolder = mappings['General']['IO']['target']
-    log_file = os.path.join(bids_path,'code','sovabids','sovaconvert.log')
+    source_path = mappings['General']['IO']['target']
+    log_file = os.path.join(bids_path,'code','sovabids','sovabids.log')
 
     # Setup the logging
-    LOGGER = setup_logging(log_file)
+    setup_logging(log_file)
     LOGGER.info('')
     LOGGER.info(SECTION_STRING + ' START CONVERT_THEM ' + SECTION_STRING)
-    LOGGER.info(f"source_path={rawfolder} targetfolder={bids_path} bidsmap={str(mapping_file)} ")
+    LOGGER.info(f"source_path={source_path} bids_path={bids_path} mapping_file={str(mapping_file)} ")
 
-    for mapping in mappings['Individual']:
-        apply_rules_to_single_file(mapping['IO']['source'],mapping,bids_path,write=True,logger=LOGGER)
-    
+    LOGGER.info(f"Converting Individual Mappings")
+    num_files = len(mappings['Individual'])
+    for i,mapping in enumerate(mappings['Individual']):
+        input_file=deep_get(mapping,'IO.source',None)
+        LOGGER.info(f"File {i+1} of {num_files} ({(i+1)*100/num_files}%) : {input_file}")
+
+        apply_rules_to_single_file(input_file,mapping,bids_path,write=True)
+
+    LOGGER.info(f"Conversion Done!")
+
+    LOGGER.info(f"Updating Dataset Description")
+
     # Grab the info from the last file to make the dataset description
     if 'dataset_description' in mappings['General']:
         dataset_description = mappings['General']['dataset_description']
         update_dataset_description(dataset_description,bids_path)
+
+    LOGGER.info(f"Dataset Description Updated!")
+
+    LOGGER.info(SECTION_STRING + ' END CONVERT_THEM ' + SECTION_STRING)
 
 
 def sovaconvert():

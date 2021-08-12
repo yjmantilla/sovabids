f35afaf8909e2ca6cd80bb972af9c475915f5e46
Yorguin José Mantilla Ramos <36543115+yjmantilla@users.noreply.github.com>
2021-08-12 00:09:27 -0500

    fix validation bug

--- tests/test_web_validator.sh
+++ tests/test_web_validator.sh
@@ -2,13 +2,13 @@
 set -e
 npm install --global npm@^7
 npm install -g bids-validator
-bids-validator _data/DUMMY/DUMMY_BIDS_custom_python
+bids-validator _data/DUMMY/DUMMY_BIDS_placeholder_python
 bids-validator _data/DUMMY/DUMMY_BIDS_regex_python
-bids-validator _data/DUMMY/DUMMY_BIDS_custom_cli
+bids-validator _data/DUMMY/DUMMY_BIDS_placeholder_cli
 bids-validator _data/DUMMY/DUMMY_BIDS_regex_cli
-bids-validator _data/DUMMY/DUMMY_BIDS_custom_rpc
+bids-validator _data/DUMMY/DUMMY_BIDS_placeholder_rpc
 bids-validator _data/DUMMY/DUMMY_BIDS_regex_rpc
-bids-validator _data/DUMMY/DUMMY_BIDS_custom_cli
+bids-validator _data/DUMMY/DUMMY_BIDS_placeholder_cli
 bids-validator _data/DUMMY/DUMMY_BIDS_regex_cli
 
 bids-validator _data/dummy_bidscoin_output

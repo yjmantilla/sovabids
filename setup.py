fc4728a03679e1bfb3733bc7b45c976ec7fdc95f
Yorguin José Mantilla Ramos <36543115+yjmantilla@users.noreply.github.com>
2021-08-20 18:46:07 -0500

    versioneer implementation

--- setup.py
+++ setup.py
@@ -1,28 +1,23 @@
 import setuptools
-
-with open("README.rst", "r", encoding="utf-8") as fh:
-    long_description = fh.read()
+import versioneer
 
 REQUIREMENTS = [i.strip() for i in open("requirements.txt").readlines()]
 
+
+# Give setuptools a hint to complain if it's too old a version
+# 30.3.0 allows us to put most metadata in setup.cfg
+# 38.3.0 contains most setup.cfg bugfixes
+# Should match pyproject.toml
+SETUP_REQUIRES = ["setuptools >= 38.3.0"]
+
+
 setuptools.setup(
-    name="sovabids",
-    version="0.0.1",
-    author="Yorguin Mantilla",
-    description="Automated eeg2bids conversion",
-    long_description=long_description,
-    long_description_content_type="text/x-rst",
-    packages=setuptools.find_packages(),
-    #install_requires = ['mne_bids','requests','pybv','pyyaml','pandas'],
+    setup_requires=SETUP_REQUIRES,
     install_requires = REQUIREMENTS,
-    classifiers=[
-        "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: MIT License",
-        "Operating System :: OS Independent",
-    ],
-    python_requires='>=3.6',
     entry_points = {'console_scripts':[
         'sovapply = sovabids.rules:sovapply',
         'sovaconvert = sovabids.convert:sovaconvert'
-        ]}
+        ]},
+        version=versioneer.get_version(),
+        cmdclass=versioneer.get_cmdclass(),
 )

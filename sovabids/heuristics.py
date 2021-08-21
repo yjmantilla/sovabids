d95d18665ef7e0a028ec01ca6f5e4539f2699668
Yorguin José Mantilla Ramos <36543115+yjmantilla@users.noreply.github.com>
2021-08-20 21:24:36 -0500

    validate the target example is a valid bids directory in the heuristic

--- sovabids/heuristics.py
+++ sovabids/heuristics.py
@@ -4,6 +4,8 @@ Functions should return a dictionary.
 """
 from sovabids.parsers import parse_path_pattern_from_entities
 from sovabids.parsers import parse_entities_from_bidspath
+from sovabids.parsers import find_bidsroot
+from bids_validator import BIDSValidator
 
 def from_io_example(sourcepath,targetpath):
     """Get the path pattern from a source-target mapping example.
@@ -22,6 +24,23 @@ def from_io_example(sourcepath,targetpath):
             'pattern': The path pattern in placeholder format.
         }
     """
+    # TODO: Should  we also allow the user to just populate the bids stuff by himself
+    # That is, instead of providing the target , directly provide the values
+    # of the entities he expects on a dictionary
+    # In example:  source = 'data/lemon/V001/resting/010002.vhdr'
+    # target = {'subject':'010002','task':'resting','session':'001'}
+    validator = BIDSValidator()
+    # Find the root since we need it relative to the bidspath for the validator to work
+    try:
+        bidsroot = find_bidsroot(targetpath)
+    except:
+        raise IOError(f'targetpath :{targetpath} is not a valid bidspath.')
+    targetpath2 = targetpath.replace(bidsroot,'') # avoid side-effects explicitly, although this shouldnt affect the str as it is immutable
+    targetpath2 = targetpath2.replace('\\','/')
+    # add / prefix
+    if targetpath2[0] != '/':
+        targetpath2 = '/' + targetpath2
+    assert validator.is_bids(targetpath2),'ERROR: The provided target-path is not a valid bids-path'
     bids_entities=parse_entities_from_bidspath(targetpath)
     pattern = parse_path_pattern_from_entities(sourcepath,bids_entities)
     return {'pattern':pattern}

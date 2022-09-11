cb87293641f974a503500701283f032662e8374e
Yorguin José Mantilla Ramos <36543115+yjmantilla@users.noreply.github.com>
2022-09-11 09:52:10 -0500

    web validator test bug fix

--- tests/test_web_validator.sh
+++ tests/test_web_validator.sh
@@ -2,14 +2,14 @@
 set -e
 npm install --global npm@^7
 npm install -g bids-validator
-bids-validator _data/DUMMY_BIDS_placeholder_cli_vhdr/
+bids-validator _data/DUMMY/DUMMY_BIDS_placeholder_cli_vhdr/
 # bids-validator _data/DUMMY_BIDS_placeholder_python_cnt/ 
 # apparently it cannot download the cnt
-bids-validator _data/DUMMY_BIDS_placeholder_python_vhdr/
-bids-validator _data/DUMMY_BIDS_placeholder_rpc_vhdr/
-bids-validator _data/DUMMY_BIDS_regex_cli_vhdr/
-bids-validator _data/DUMMY_BIDS_regex_python_vhdr/
-bids-validator _data/DUMMY_BIDS_regex_rpc_vhdr/
+bids-validator _data/DUMMY/DUMMY_BIDS_placeholder_python_vhdr/
+bids-validator _data/DUMMY/DUMMY_BIDS_placeholder_rpc_vhdr/
+bids-validator _data/DUMMY/DUMMY_BIDS_regex_cli_vhdr/
+bids-validator _data/DUMMY/DUMMY_BIDS_regex_python_vhdr/
+bids-validator _data/DUMMY/DUMMY_BIDS_regex_rpc_vhdr/
 
 
 bids-validator _data/dummy_bidscoin_output

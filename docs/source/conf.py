0b1fee445f7f74fbac24f56d38a9bcade69a3128
yjmantilla <yjmantilla@gmail.com>
2025-03-18 02:56:54 -0400

    Reintroduce autoapi and viewcode extensions in Sphinx configuration

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -40,8 +40,6 @@ extensions = [
     "myst_parser",
     "sphinx.ext.autodoc",
     "sphinx.ext.autosummary",
-    'autoapi.extension', #see https://github.com/readthedocs/sphinx-autoapi/issues/422
-    "sphinx.ext.viewcode",
 #    "sphinx.ext.intersphinx",
     #"numpydoc",
     "sphinx_gallery.gen_gallery",
@@ -107,5 +105,9 @@ def setup(app):
    app.add_css_file('gallery.css')
 
 # Auto API
+extensions += ['autoapi.extension']
+
 autoapi_type = 'python'
 autoapi_dirs = ["../../sovabids"]
+
+extensions += ['sphinx.ext.viewcode'] #see https://github.com/readthedocs/sphinx-autoapi/issues/422
